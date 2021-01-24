fa3a1432e3dd16066739a21c2c6253304418c38e
Amos Mogaka <mogaka.amo254@gmail.com>
2021-01-24 21:18:22 +0300

    Working on the script

---
+++ script.js
@@ -0,0 +1,29 @@
+function generateRandomNum(num) {
+    return Math.floor(Math.random() * num)
+}
+
+const codeMessage = {
+    codeInfo: ['success', 'error', 'not working', 'gross'],
+    output: ['You can have a break', 'debug it please', 'dude just try again!!!', 'work on your code'],
+}
+
+let result = []
+
+for(let prop in codeMessage) {
+    let optionIdx = generateRandomNum(codeMessage[prop].length)
+
+
+    switch (prop) {
+      case 'error': 
+        result.push(`Hey, "${codeMessage[prop][optionIdx]}".`)
+        break;
+      case 'success':
+          result.push(`Hey, "${codeMessage[prop][optionIdx]}"`)
+          break;
+      case 'not working':
+          break;
+      case 'gross':
+          break;
+}
+
+     
